21091e2cef96e68a56da4b880bb3df86c4fed06c
Low Wei Hong <M140042@e.ntu.edu.sg>
2025-12-30 11:13:00 -0800

    Refactor HTTP headers for US News request
    Removed unnecessary headers and updated User-Agent.

--- university_ranking_scraper/constant.py
+++ university_ranking_scraper/constant.py
@@ -43,17 +43,11 @@ SUBJECTS = {
 # Headers for HTTP requests
 HEADERS = {
     'usnews': {
-        'accept': '*/*',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-        'priority': 'u=1, i',
-        'referer': 'https://www.usnews.com/',
-        'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36',
+        'sec-ch-ua': '"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
     },
     'times': {
         'sec-ch-ua-platform': '"macOS"',
